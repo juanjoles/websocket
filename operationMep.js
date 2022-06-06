70d1b5f69c12c7e668d7fe5400db9011e1a5aaa3
jlescano <jlescano@auntap.com>
2022-06-06 18:12:07 -0300

    gitignore ok

--- operationMep.js
+++
@@ -1,6 +0,0 @@
-module.exports = 
-    function(priceArs,priceUsd,symbolArs,symbolUsd,settlementType)  {
-    let mep = priceArs / priceUsd;
-    console.log("Valor dolar MEP", settlementType," -", symbolArs, " - ",symbolUsd , mep)
-}
-
